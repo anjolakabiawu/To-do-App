8c7019f7024e4b4bb1f7759e2347b5ccacad509f
Evangel Ubaru <anjolakabiawu@yahoo.com>
2025-05-05 18:26:58 +0100

    Working on the Todo page

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from todo import models
 from todo.models import TODOO
+from django.contrib.auth import authenticate,login,logout
 
 
 def signup(request):
@@ -14,3 +15,16 @@ def signup(request):
         my_user.save()
         return redirect('/login')        
     return render(request, 'sign.html')
+
+def login(request):
+    if request.method=='POST':
+        fnm =request.POST.get('fnm')
+        pwd = request.POST.get('pwd')
+        print(fnm, pwd)
+        user = authenticate(request, username=fnm, password=pwd)
+        if user is not None:
+            login(request, user)
+            return redirect('/todopage')
+        else:
+            return redirect('/login')
+    return render(request, "login.html")
